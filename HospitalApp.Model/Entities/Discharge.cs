216d686fb6414e8e06809c0e9bc77e54c3cc4d5b
larrybloom <aniediabasi.joseph@decagon.dev>
2024-03-02 09:28:56 +0100

    jk

--- HospitalApp.Model/Entities/Discharge.cs
+++ HospitalApp.Model/Entities/Discharge.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HospitalApp.Model.Entities
+﻿namespace HospitalApp.Model.Entities
 {
     public class Discharge
     {
